097b19a6a95d6d6f747e778d5edac337ab0a5fbe
Krzysztof Kapusta <krz.kapusta@gmail.com>
2013-09-22 12:22:31 +0200

    Clean main

---
+++ hwinit.h
@@ -0,0 +1,11 @@
+#ifndef _HWINIT_H_
+#define _HWINIT_H_
+
+/**
+ * \brief Initialize all hardware.
+ *
+ * THIS FUNCTION DISABLES INTERRUPTS AND DO NOT ENABLES THEM AGAIN
+ */
+void Initialize();
+
+#endif
